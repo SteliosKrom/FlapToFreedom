dd6521d65716f2f85ffe249c6c77c52dae92e0f0
SteliosKrom <stelios.yolokrom@gmail.com>
2024-02-28 11:11:31 +0200

    Change

--- Assets/Scripts/MoveBackgroundLeft.cs
+++ Assets/Scripts/MoveBackgroundLeft.cs
@@ -6,20 +6,10 @@ public class MoveBackgroundLeft : MonoBehaviour
 {
     public float speed = 3f;
 
-    private PlayerController playerController;
-    private UIManager uiManager;
-    private GameManager gameManager;
-
     // Start is called before the first frame update
     void Start()
     {
-        if (playerController != null)
-        {
-            playerController = GameObject.Find("PlayerController").GetComponent<PlayerController>();
-        }
 
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
     }
 
     // Update is called once per frame
