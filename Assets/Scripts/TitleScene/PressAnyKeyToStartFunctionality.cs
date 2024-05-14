7b8cb56f055349a5a19fa342524b4c0d47463ea3
SteliosKrom <stelios.yolokrom@gmail.com>
2024-05-14 16:30:17 +0300

    Push Changes

--- Assets/Scripts/TitleScene/PressAnyKeyToStartFunctionality.cs
+++ Assets/Scripts/TitleScene/PressAnyKeyToStartFunctionality.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
-
 public class PressAnyKeyToStartFunctionality : MonoBehaviour
 {
     public bool isPressed = false;
